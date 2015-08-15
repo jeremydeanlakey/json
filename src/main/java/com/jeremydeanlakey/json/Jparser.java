14d7d09cc9bc52ba5b6d18ac9d1a5eb0e6855037
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2015-08-15 07:53:13 -0600

    begin work on parser

---
+++ src/main/java/com/jeremydeanlakey/json/Jparser.java
@@ -0,0 +1,15 @@
+package com.jeremydeanlakey.json;
+
+/**
+ * Created by jeremydeanlakey on 8/15/15.
+ */
+public class Jparser {
+    String src;
+    int location = 0;
+
+    public static Json stringToJson(String src) { return (new Jparser(src)).getJson(); }
+
+    public Jparser(String src) { this.src = src; }
+
+    public Json getJson() { return null; } // TODO
+}
