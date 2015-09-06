2d731c58059706827f7ebc999729b14a1066b91e
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2015-09-06 07:56:49 -0600

    start work on JparserTest

---
+++ src/androidTest/java/com/jeremydeanlakey/json/JparserTest.java
@@ -0,0 +1,43 @@
+package com.jeremydeanlakey.json;
+
+import android.test.AndroidTestCase;
+
+import junit.framework.Assert;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by jeremydeanlakey on 6/22/15.
+ */
+public class JparserTest extends AndroidTestCase {
+    public static String TEST_CASE_STRING =
+            "{"
+        +    "\"emptyArray\":[],"
+        +    "\"nonEmptyArray\":[{\"contents\": 1}, 2, false, null, \"string\", []],"
+        +    "\"string\": \"I am a string\","
+        +    "\"numbers\":[-2e0, -1.000, 0e10, 10e-1, 2],"
+        +    "\"bigNumber\": " + Integer.MAX_VALUE + ","
+        +    "\"null\": null,"
+        +    "\"boolean\": false,"
+        +    "\"emptyObject\": {},"
+        +    "\"notObject\": 1,"
+        +    "\"nonEmptyObject\": {\"contents\":1},"
+        +    "\"testObject\": {"
+        +        "\"number\":1,"
+        +        "\"string\":\"string\","
+        +        "\"bool\":\"true\","
+        +        "\"null\":null,"
+        +        "\"array\":[],"
+        +        "\"object\":{}"
+        +      "}"
+        +    "}";
+
+
+    public void testStringToJson() throws Throwable {
+        
+    }
+}
