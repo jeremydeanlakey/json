18b6871f559812afe5860550ab4e9079f81efd81
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2015-10-19 03:07:51 -0600

    add getBoolean to Jstring to handle string false and true

--- src/main/java/com/jeremydeanlakey/json/Jstring.java
+++ src/main/java/com/jeremydeanlakey/json/Jstring.java
@@ -12,6 +12,11 @@ public class Jstring extends Json {
     @Override public boolean isString() { return true; }
 
     @Override public String getString(){ return value; }
+    public boolean getBoolean(){
+        if (value.equalsIgnoreCase("true")) return true;
+        else if (value.equalsIgnoreCase("false")) return false;
+        else throw new RuntimeException("Not a boolean value");
+    }
 
     @Override public String toString() { return "\"" + value + "\""; }
     @Override protected void writeTo(StringBuilder builder) { builder.append(QUOTE).append(value).append(QUOTE); }
