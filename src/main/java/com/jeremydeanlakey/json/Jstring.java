fd98cf9228e813804c0820dae202cab4de2fd68f
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-02-20 09:20:55 -0700

    reorganize Jstring

--- src/main/java/com/jeremydeanlakey/json/Jstring.java
+++ src/main/java/com/jeremydeanlakey/json/Jstring.java
@@ -9,23 +9,22 @@ public class Jstring extends Json {
 
     protected Jstring(String value){ this.value = value; }
 
-    @Override public boolean isString() { return true; }
+    @Override public boolean equals(Object o) { return (o instanceof Jstring) && ((Jstring)o).getString().equals(value); }
 
-    @Override public String getString(){ return value; }
-    @Override public boolean isBoolean() { return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false"); }
+    @Override public boolean isString() { return true; }
     @Override public boolean isNull() { return value.equalsIgnoreCase("null"); }
+    @Override public boolean isBoolean() { return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false"); }
     @Override public boolean isNumber() { try { Double.valueOf(value); return true; } catch (Exception e) { return false;} }
 
+    @Override public String getString(){ return value; }
     @Override public boolean getBoolean(){
         if (value.equalsIgnoreCase("true")) return true;
         else if (value.equalsIgnoreCase("false")) return false;
         else throw new RuntimeException(Json.NOT_BOOLEAN);
     }
-    @Override public double getDouble() { try { return Double.valueOf(value); } catch (Exception e) { throw new RuntimeException("Not a number");} }
     @Override public long getLong() { try { return Long.valueOf(value); } catch (Exception e) { throw new RuntimeException("Not a number");} }
+    @Override public double getDouble() { try { return Double.valueOf(value); } catch (Exception e) { throw new RuntimeException("Not a number");} }
 
     @Override public String toString() { return "\"" + value + "\""; }
     @Override protected void writeTo(StringBuilder builder) { builder.append(QUOTE).append(value).append(QUOTE); }
-
-    @Override public boolean equals(Object o) { return (o instanceof Jstring) && ((Jstring)o).getString().equals(value); }
 }
