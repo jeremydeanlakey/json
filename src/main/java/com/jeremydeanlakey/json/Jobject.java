0ed2b4b6e03f4601113025e11d3d2daa8d64a788
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-02-23 07:20:46 -0700

    reorganize Jobject methods to be in same order as Jarry

--- src/main/java/com/jeremydeanlakey/json/Jobject.java
+++ src/main/java/com/jeremydeanlakey/json/Jobject.java
@@ -9,15 +9,25 @@ import java.util.Set;
  * Created by jeremydeanlakey on 6/23/15.
  */
 public class Jobject extends Json {
-    private Map<String, Json> map;
     private static final String QUOTE = "\"";
     protected static final String UPDATE_ERROR = "update(Json) requires an object as argument";
-    protected Jobject(){map = new HashMap();}
 
+    private Map<String, Json> map;
+
+    protected Jobject(){ map = new HashMap(); }
+
+    @Override public Set<String> keys() { return map.keySet(); }
+    @Override public void put(String key, Json value){map.put(key, value);}
+    @Override public int length() { return map.size(); }
+    @Override public void update(Json object) {
+        if (object == null || !Json.isObject(object))
+            throw new RuntimeException(UPDATE_ERROR);
+        for (String key: object.keys())
+            put(key, object.get(key));
+    }
 
     @Override public boolean isEmpty() {return map.isEmpty(); }
     @Override public boolean isObject() { return true;}
-
     @Override public boolean isNull(String key) { return map.get(key).isNull(); }
     @Override public boolean isBoolean(String key) { return map.get(key).isBoolean(); }
     @Override public boolean isLong(String key) { return map.get(key).isNumber(); }
@@ -25,7 +35,6 @@ public class Jobject extends Json {
     @Override public boolean isString(String key) { return map.get(key).isString(); }
     @Override public boolean isObject(String key) { return map.get(key).isObject(); }
     @Override public boolean isArray(String key) { return map.get(key).isArray(); }
-
 
     @Override public boolean has(String key){return map.containsKey(key);}
     @Override public boolean hasNull(String key) { return has(key) && isNull(key); }
@@ -36,7 +45,6 @@ public class Jobject extends Json {
     @Override public boolean hasObject(String key){ return has(key) && isObject(key); }
     @Override public boolean hasArray(String key){ return has(key) && isArray(key); }
 
-
     @Override public Json get(String key){return map.get(key);}
 
     @Override public boolean getBoolean(String key){ return map.get(key).getBoolean(); }
@@ -45,7 +53,6 @@ public class Jobject extends Json {
     @Override public String getString(String key){ return map.get(key).getString(); }
     @Override public Json getArray(String key){ if (!map.get(key).isArray()) throw new RuntimeException(NOT_ARRAY); return map.get(key); }
     @Override public Json getObject(String key){ if (!map.get(key).isObject()) throw new RuntimeException(NOT_OBJECT); return map.get(key); }
-
     @Override public Json get(String key, Json def){return has(key) ? get(key) : def;}
     @Override public boolean getBoolean(String key, boolean def){ return hasBoolean(key) ? getBoolean(key) : def; }
     @Override public long getLong(String key, long def){ return hasLong(key) ? getLong(key) : def; }
@@ -53,18 +60,6 @@ public class Jobject extends Json {
     @Override public String getString(String key, String def){ return hasString(key) ? getString(key) : def; }
     @Override public Json getArray(String key, Jarray def){ return hasArray(key) ? get(key) : def; }
     @Override public Json getObject(String key, Jobject def){ return hasObject(key) ? get(key) : def; }
-
-
-    @Override public Set<String> keys() { return map.keySet(); }
-    @Override public void put(String key, Json value){map.put(key, value);}
-    @Override public int length() { return map.size(); }
-
-    @Override public void update(Json object) {
-        if (object == null || !Json.isObject(object))
-            throw new RuntimeException(UPDATE_ERROR);
-        for (String key: object.keys())
-            put(key, object.get(key));
-    }
 
     @Override
     public void writeTo(StringBuilder builder) {
