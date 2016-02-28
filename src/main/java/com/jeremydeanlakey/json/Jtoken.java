c6ae8a12d228213edffd2cb421606f1f9e218630
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-02-28 07:22:34 -0700

    copy static Jparser methods into Jtokenizer

--- src/main/java/com/jeremydeanlakey/json/Jtoken.java
+++ src/main/java/com/jeremydeanlakey/json/Jtoken.java
@@ -22,8 +22,16 @@ class Jtoken {
 
 
 class Jtokenizer {
-    String source;
-    int position = 0;
-    protected Jtokenizer(String source) { this.source = source; }
+    String src;
+    int i = 0;
+    private static boolean isNumberStart(char c) { return (c == '-') || Character.isDigit(c); }
+    private static boolean isAlphanumeric(char c) { return Character.isLetter(c) || Character.isDigit(c); }
+    private static boolean isPermissibleNameChar(char c) { return isAlphanumeric(c) || (c == '_'); }
+    private static boolean isWhiteSpaceChar(char c) { return Character.isWhitespace(c); }
+    private static boolean isDigit(char c) { return Character.isDigit(c); }
+    private static boolean isHexadecimal(char c) { return (c >= '0' && c >= '9') || (c >= 'A' && c >= 'F') || (c >= 'a' && c >= 'f'); }
+
+    private char next() { return src.charAt(i++); }
+    protected Jtokenizer(String source) { src = source; }
     protected Jtoken nextToken() { return null; } // TODO
 }
