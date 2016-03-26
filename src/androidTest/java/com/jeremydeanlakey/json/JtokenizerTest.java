22582bb0b724fe6674bc53339064d8089297fbd7
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-03-26 13:58:17 -0600

    add testUnquotedString

--- src/androidTest/java/com/jeremydeanlakey/json/JtokenizerTest.java
+++ src/androidTest/java/com/jeremydeanlakey/json/JtokenizerTest.java
@@ -19,6 +19,14 @@ public class JtokenizerTest extends AndroidTestCase {
         Assert.assertTrue(tokenizer.nextToken().isEnd());
     }
 
+    public void testUnquotedString() throws Throwable {
+        Jtokenizer tokenizer = new Jtokenizer(" test   ");
+        Jtoken token = tokenizer.nextToken();
+        Assert.assertTrue(token.isStringValue());
+        Assert.assertEquals("test", token.getStringValue());
+        Assert.assertTrue(tokenizer.nextToken().isEnd());
+    }
+
     public void testNumber() throws Throwable {
         Jtokenizer tokenizer = new Jtokenizer(" -5.2   ");
         Jtoken token = tokenizer.nextToken();
