743206a813586f6ad067a1c817cc76c718ab5eae
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-04-14 18:27:01 -0600

    convert char

--- src/main/java/com/jeremydeanlakey/json/Jtokenizer.java
+++ src/main/java/com/jeremydeanlakey/json/Jtokenizer.java
@@ -114,6 +114,19 @@ class Jtokenizer {
         }
     }
 
+    private char convertEscapedChar(char c) {
+        switch (c) {
+            case '"': return '"';
+            case '\\': return '\\';
+            case 'b': return '\b';
+            case 'f': return '\f';
+            case 'n': return '\n';
+            case 'r': return '\r';
+            case 't': return '\t';
+            default: throw makeException("escapable char", c);
+        }
+    }
+
     private String getQuotedString() {
         allowWhiteSpaceAndComments();
         char c = requireQuote();
