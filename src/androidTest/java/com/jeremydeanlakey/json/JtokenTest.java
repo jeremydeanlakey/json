0fb884d63d81a6dd52cc34e8b82b144c65aaa80d
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-05-05 07:48:30 -0600

    create token test class

---
+++ src/androidTest/java/com/jeremydeanlakey/json/JtokenTest.java
@@ -0,0 +1,26 @@
+package com.jeremydeanlakey.json;
+
+import android.test.AndroidTestCase;
+
+import junit.framework.Assert;
+
+/**
+ * Created by jeremydeanlakey on 4/5/16.
+ */
+public class JtokenTest extends AndroidTestCase {
+
+    public void setUp() {
+    }
+
+    // TODO: number
+    // TODO: string
+    // TODO: json
+    // TODO: {
+    // TODO: :
+    // TODO: }
+    // TODO: [
+    // TODO: ]
+    // TODO: ,
+    // TODO: end
+
+}
