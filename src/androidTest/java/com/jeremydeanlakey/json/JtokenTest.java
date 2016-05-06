76454ebcd9e6f77088bd1adab04db312d1a6142a
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-05-06 07:50:41 -0600

    first half of testNumber

--- src/androidTest/java/com/jeremydeanlakey/json/JtokenTest.java
+++ src/androidTest/java/com/jeremydeanlakey/json/JtokenTest.java
@@ -12,6 +12,27 @@ public class JtokenTest extends AndroidTestCase {
     public void setUp() {
     }
 
+    public void testNumber() throws Throwable {
+        Jtokenizer tokenizer = new Jtokenizer(" 5.3   ");
+        Jtoken token = tokenizer.nextToken();
+
+        Assert.assertTrue(token.isNumber());
+        Assert.assertFalse(token.isStringValue());
+        Assert.assertFalse(token.isJsonValue());
+        Assert.assertFalse(token.isObjectStart());
+        Assert.assertFalse(token.isObjectEnd());
+        Assert.assertFalse(token.isColon());
+        Assert.assertFalse(token.isArrayStart());
+        Assert.assertFalse(token.isArrayEnd());
+        Assert.assertFalse(token.isComma());
+        Assert.assertFalse(token.isEnd());
+
+        // isValidToken(c)
+        // toString()
+        // getNumberValue());
+        // getStringValue());
+        // getJsonValue());
+    }
     // TODO: number
     // TODO: string
     // TODO: json
