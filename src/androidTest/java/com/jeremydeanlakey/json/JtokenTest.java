9c7480eb74f42c3f80d612dbc5c14a14e4cba495
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-05-08 08:32:34 -0600

    add string test

--- src/androidTest/java/com/jeremydeanlakey/json/JtokenTest.java
+++ src/androidTest/java/com/jeremydeanlakey/json/JtokenTest.java
@@ -30,12 +30,32 @@ public class JtokenTest extends AndroidTestCase {
         // isValidToken(c)
         Assert.assertEquals(token.toString(), "5.3");
         Assert.assertEquals(token.getNumberValue(), 5.3);
-        // Assert.assertEquals(getStringValue(), whatever));
-        // Assert.assertEquals(getJsonValue(), whatever));
+        // Assert.assertEquals(token.getStringValue(), whatever);
+        // Assert.assertEquals(token.getJsonValue(), whatever);
     }
-    
-    // TODO: number
-    // TODO: string
+
+    public void testString() throws Throwable {
+        Jtokenizer tokenizer = new Jtokenizer(" \"I'm a string!\"   ");
+        Jtoken token = tokenizer.nextToken();
+
+        Assert.assertFalse(token.isNumber());
+        Assert.assertTrue(token.isStringValue());
+        Assert.assertFalse(token.isJsonValue());
+        Assert.assertFalse(token.isObjectStart());
+        Assert.assertFalse(token.isObjectEnd());
+        Assert.assertFalse(token.isColon());
+        Assert.assertFalse(token.isArrayStart());
+        Assert.assertFalse(token.isArrayEnd());
+        Assert.assertFalse(token.isComma());
+        Assert.assertFalse(token.isEnd());
+
+        // isValidToken(c)
+        Assert.assertEquals(token.toString(), "I'm a string!");
+        //Assert.assertEquals(token.getNumberValue(), 5.3);
+        Assert.assertEquals(token.getStringValue(), "I'm a string!");
+        // Assert.assertEquals(token.getJsonValue(), whatever);
+    }
+
     // TODO: json
     // TODO: {
     // TODO: :
