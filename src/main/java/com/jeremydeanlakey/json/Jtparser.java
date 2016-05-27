b83ed00edcc5dd0356faa8b913f2c4ed8a5df666
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-05-27 08:40:26 -0600

    add helper functions and static strings

--- src/main/java/com/jeremydeanlakey/json/Jtparser.java
+++ src/main/java/com/jeremydeanlakey/json/Jtparser.java
@@ -10,7 +10,21 @@ import java.util.List;
  * Created by jeremydeanlakey on 5/01/16.
  */
 public class Jtparser {
+    private static final String TAG = "Jtparser";
+    private static final String END = "END";
+    private static final String NOT_END = "NOT END";
+    private static final String EXCEPTION = "Unexpected character: '%s'.  Expected: '%s' at %d.";
+
     private Jtokenizer tokenizer;
+
+    public class JparserException extends RuntimeException {
+        JparserException(String message) { super(message); }
+    }
+
+    // TODO should tokenizer do previous position?
+    private String exceptionMessage(Jtoken expected, Jtoken actual) { return String.format(EXCEPTION, actual, expected, tokenizer.position()); }
+    private JparserException makeException(Jtoken expected, Jtoken actual) { return new JparserException(exceptionMessage(expected, actual)); }
+
 
     public static Json stringToJson(String src) { return (new Jtparser(src)).getJson(); }
 
