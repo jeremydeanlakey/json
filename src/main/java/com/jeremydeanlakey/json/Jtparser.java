554dc6a66e15fd3c736af5b05c05009f320d7bc3
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-06-26 07:57:30 -0600

    remove unused imports

--- src/main/java/com/jeremydeanlakey/json/Jtparser.java
+++ src/main/java/com/jeremydeanlakey/json/Jtparser.java
@@ -1,10 +1,6 @@
 package com.jeremydeanlakey.json;
 
-import android.util.Log;
 import android.util.Pair;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by jeremydeanlakey on 5/01/16.
